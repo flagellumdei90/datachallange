1f083d2befde6264f620a5979219cffcdb456a80
Gulyati <gulyati@gmail.com>
2023-03-13 23:43:09 +0100

    20230313_v2

--- 03_model/BellaVita.R
+++ 03_model/BellaVita.R
@@ -1,31 +1,29 @@
 # Load packages
+library("tidyverse") # imap function for roc curve
+library("data.table") # to use data in an efficient way
+library("readxl") # to read xlsx data
+library("caret") # to split data into train and holdput sets
+library("dplyr") # to append commands easier
+library("ROCR") # to evaluate model performance
 
-# Load home win models
-home_model_rf <- readRDS(paste0(output,"home_model_rf.rda"))
-home_model_glm <- readRDS(paste0(output,"home_model_glm.rda"))
-home_model_gbm <- readRDS(paste0(output,"home_model_gbm.rda"))
-home_model_xgb <- readRDS(paste0(output,"home_model_xgb.rda"))
-home_model_nb <- readRDS(paste0(output,"home_model_nb.rda"))
-home_model_knn <- readRDS(paste0(output,"home_model_knn.rda"))
-home_model_svm <- readRDS(paste0(output,"home_model_svm.rda"))
 
-link_home_glm <- "https://github.com/flagellumdei90/datachallange/blob/main/04_output/home_model_glm.rda?raw=true"
-
-home_model_glm <- readRDS(url(link_home_glm))
+# Load  models
+link_home_model<- "https://github.com/flagellumdei90/datachallange/blob/main/04_output/home_model_glm.rda?raw=true"
+home_model <- readRDS(url(link_home_model))
 
 # Create modell_input tables
   # Load data
   data <- as.data.table(read_excel(paste0(data_in, "competition_table.xlsx")))
   
   # Create modell_input tables
-  data_input_home <- data[,-c("home_win_flag", "draw_flag", "away_win_flag")]
+  model_input <- data[,-c("home_win_flag", "draw_flag", "away_win_flag")]
   
-  # Feature engineering
 
 # Predict
-predicted_proba_holdout <- predict(home_model_glm, 
-                                   newdata = data_input_home, 
+predicted_home_win <- predict(home_model, 
+                                   newdata = model_input, 
                                    type = "prob")[["yes"]]
+
 
 
 # Output - 3 táblát állítson elő
